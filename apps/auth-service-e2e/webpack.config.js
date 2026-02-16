e0a211839036be9a38308e9c19cb22349d76a983
shayandutta <shayandutta23@gmail.com>
2026-02-16 17:18:11 +0530

    feat: figure out attempt for CI checks

--- apps/auth-service-e2e/webpack.config.js
+++ apps/auth-service-e2e/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
       compiler: 'tsc',
       main: './src/main.ts',
       tsConfig: './tsconfig.app.json',
+      assets: [],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
