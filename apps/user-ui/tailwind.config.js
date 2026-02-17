a90c9f52f0b5f05954eb3a0d5b91999fdecbbc06
shayandutta <shayandutta23@gmail.com>
2026-02-17 16:59:03 +0530

    feat: header

--- apps/user-ui/tailwind.config.js
+++ apps/user-ui/tailwind.config.js
@@ -1,13 +1,3 @@
-// const { createGlobPatternsForDependencies } = require('@nx/next/tailwind');
-
-// The above utility import will not work if you are using Next.js' --turbo.
-// Instead you will have to manually add the dependent paths to be included.
-// For example
-// ../libs/buttons/**/*.{ts,tsx,js,jsx,html}',                 <--- Adding a shared lib
-// !../libs/buttons/**/*.{stories,spec}.{ts,tsx,js,jsx,html}', <--- Skip adding spec/stories files from shared lib
-
-// If you are **not** using `--turbo` you can uncomment both lines 1 & 19.
-// A discussion of the issue can be found: https://github.com/nrwl/nx/issues/26510
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -17,7 +7,12 @@ module.exports = {
     //     ...createGlobPatternsForDependencies(__dirname)
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: 'var(--font-roboto)',
+        Poppins: 'var(--font-poppins)',
+      }
+    },
   },
   plugins: [],
 };
